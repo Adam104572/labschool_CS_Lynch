e03e71cd22b8e1edc3dc35750f5acf2e89c9e210
Adam104572 <alynch@dragonwork.com>
2024-09-12 13:56:17 +0000

    created new files

---
+++ Unti_1/strings.py
@@ -0,0 +1,24 @@
+myString = "Hello World!"
+
+#len() retuens the length of a string
+
+print(len(myString))
+
+pledge = "I pledge allegiance to the flag of the United States of America, and to the Republic for"
+
+# how many characters are here? Use the lent() function, and print the result.
+print(len(pledge))
+
+line_2 = "I like to eat, sleep, and play video games"
+
+line_3 = "Im not a fan of school"
+
+line_4 = "I play soccer"
+
+print(len(line_2))
+print(len(line_3))
+print(len(line_4))
+
+line_5 = "500 cigarettes"
+
+line_6 = "1 milion bears"
---
+++ Unti_1/variables.py
@@ -0,0 +1,15 @@
+print("good morning class!")
+
+# this is a comant. It is used for leaving comants on your code.
+
+x = 17
+y = 25
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x^y)
+print(x**y)
+
+thisIsAVariable = "Adam"
+print(thisIsAVariable)
