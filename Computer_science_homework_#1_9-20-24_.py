c055d4ed902231fc1ed9c6cd484a879de0442ee9
Adam104572 <alynch@dragonwork.com>
2024-09-20 19:15:24 -0400

    Add files via upload

---
+++ Computer_science_homework_#1_9-20-24_.py
@@ -0,0 +1,128 @@
+#Concatienation 10 examples homework for CS class - Adam M. Lynch (9-20-24)
+
+#I'm using a random word/sentenc generator so that's why there's a bunch of weird sentences getting combined
+
+#First Concatienation
+word1string = "tire"
+word2string = "fire"
+bingus = word1string +" "+ word2string
+print(bingus)
+
+#Second Concatienation
+word3string = "British"
+word4string = "has bad teeth"
+bingus2 = word3string + " " + word4string
+print(bingus2)
+#I tried to use speech to text to get tea but it gave me teeth instead so decided to kept it because I think it's funny 
+
+#Third Concatienation
+word5string = "sarlacc"
+word6string = "pit"
+bingus3 = word5string + " " + word6string
+print(bingus3)
+
+#Forth Concatienation
+word7string = "Twitter"
+word8string = "is bad"
+bingus4 = word7string + " " + word8string + "!"
+print(bingus4)
+
+#Fifth Concatienation
+word9string = "Halloween is on"
+word10sting = "October 31st"
+bingus5 = word9string + " thursday " + word10sting
+print(bingus5)
+
+#Sixth Concatienation
+word11 = "lofi"
+word12 = "music"
+bingus6 = word11 + " is a type of " + word12 + " that I like to listen to when studying."
+print(bingus6)
+
+#Seventh Concatienation
+word13 = "Texas Instruments"
+word14 = " make missiles"
+bingus7 = word13 + word14
+print(bingus7)
+
+#Eight Concatienation
+word15 = "I'm sitting in a folding chair,"
+word16 = " it is not comfortable at all!!"
+bingus8 = word15 + word16
+print(bingus8)
+
+#Nineth Concatienation
+word17 = ". . . no really"
+word18 = " this chair is garbage"
+bingus9 = word17 + word18
+print(bingus9)
+
+#Tenth Concatienation
+word19 = "I am finally done"
+word20 = " with the Concatienation"
+bingus10 = word19 + word20 + ". Yippy"
+print(bingus10)
+
+#I'm going to add a space so when it prints it's easier to see the start of the f-string
+space = " "
+print(space)
+
+#First f-string
+word21 = "52"
+word22 = " polar bears are at the zoo"
+bigFlappa = f"{word21}{word22}"
+print(bigFlappa)
+
+#Second f-string
+word23 = "this homework"
+word24 = "is taking forever to complete"
+bigFlappa2 = f"{word23} {word24}"
+print(bigFlappa2)
+
+#Third f-string
+word25 = "the homework"
+word26 = "is not necessarily hard it is just long"
+bigFlappa3 = f"{word25} {word26}"
+print(bigFlappa3)
+
+#Forth f-string
+word27 = "my dog"
+word28 = " is barking"
+bigFlappa4 = f"{word27}{word28}"
+print(bigFlappa4)
+
+#Fifth f-string
+word29 = "I am training"
+word30 = "for the Army 10 miler on"
+bigFlappa5 = f"{word29} {word30} October 13th"
+print(bigFlappa5)
+
+#Sixth f-string
+word31 = "this folding chair"
+word32 = "is going to cause me to have scoliosis"
+bigFlappa6 = f"{word31} {word32}"
+print(bigFlappa6)
+
+#Seventh f-string
+word33 = "my dog"
+word34 = "is brown"
+bigFlappa7 = f"{word33} {word34}"
+print(bigFlappa7)
+
+#Eight f-string
+word35 = "i want"
+word36 = "to eat pizza"
+bigFlappa8 = f"{word35} {word36}"
+print(bigFlappa8)
+
+#Nineth f-string
+word37 = "the winter Olympics" 
+word38 = "are going to be held in 2026"
+bigFlappa9 = f"{word37} {word38}"
+print(bigFlappa9)
+
+#Tenth f-string
+word39 = "I like Texas"
+word40 = "cinnamon rolls"
+bigFlappa10 =f"{word39} {word40}"
+print(bigFlappa10)
