3011ded85ce369ba65f9884a4bf99e2e6f22e591
Adam104572 <alynch@dragonwork.com>
2024-09-23 18:24:42 -0400

    Add files via upload

---
+++ Unit_2/Computer_science_f-string_break_apart_variables_homework_#2_ 9-23-24.py
@@ -0,0 +1,17 @@
+#Take a favorite movie scene or song and break it apart into variables. 
+#Restructure it using variables: concatenate with escape characters OR f-Strings with escape characters
+#You must have AT LEAST 8 - 12 lines of lyric lines or movie quote lines.
+
+simpsonsMovieLine = "Singers: Spider-Pig. Spider-Pig Does whatever a Spider-Pig does Look out! He's a Spider-"
+simpsonsMovieLine2 = "Medicine Woman: Unless you have an epiphany you will spend the remainder of your days alone."
+simpsonsMovieLine3 = "Homer: Epiphany, epiphany, epiphany. Bananas are an excellent source of potassium. (The ghostly hans slap him in the face) Americans will never embrace soccer? (Gets slapped again) More than two shakes and it's playing with yourself? (gets slapped again, then disembodied.) Hey, what are you doing? (His body parts begin to melt) Oh, do whatever you want to me. I don't care about myself anymore. (his body parts reverse the melting process)"
+simpsonsMovieLine4 = "Medicine Woman: Because...?"
+simpsonsMovieLine5 = "Homer: Because other people are just as important as me. Without them, I’m nothing. In order to save myself I have to save Springfield! That's it! Isn't it?"
+simpsonsMovieLine6 = "[Homer is acclaimed by applause and cheering before Homer wakes from the vision.]"
+simpsonsMovieLine7 = "Homer: That was the most incredible experience of my life. And now to find my family, save my town and drop 10 pounds! Thank you, boob lady."
+simpsonsMovieLine8 = "[Homer hugs the Medicine Woman and walks away. The train arrives at Seattle and family lower the head when they see Russ Cargill.]"
+simpsonsMovieLine9 = "Marge: This is it, kids. Seattle. Russ Cargill! Do you think he saw us?"
+simpsonsMovieLine10 = "[EPA enters the car and take care of them.]"
+simpsonsMovieLine11 = "Russ Cargill: Yes, I did."
+simpsonsMovieDialogue = f"{simpsonsMovieLine}\n{simpsonsMovieLine2}\n{simpsonsMovieLine3}\n{simpsonsMovieLine4}\n{simpsonsMovieLine5}\n{simpsonsMovieLine6}\n{simpsonsMovieLine7}\n{simpsonsMovieLine8}\n{simpsonsMovieLine9}\n{simpsonsMovieLine10}\n{simpsonsMovieLine11}"
+print(simpsonsMovieDialogue)
